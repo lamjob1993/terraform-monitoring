fcce7e91be7af63fe436ab546913f8fbcfc613a6
lamjob1993 <43383780+lamjob1993@users.noreply.github.com>
2025-05-08 21:44:44 +0300

    Update variables.tf

--- terraform/tasks/terraform_project/modules/environment/variables.tf
+++ terraform/tasks/terraform_project/modules/environment/variables.tf
@@ -1,1 +1,19 @@
+variable "env_name" {
+  description = "Name of the environment (dev, staging, prod)"
+  type        = string
+}
 
+variable "web_port" {
+  description = "External port for web server"
+  type        = number
+}
+
+variable "db_user" {
+  description = "PostgreSQL user"
+  type        = string
+}
+
+variable "db_password" {
+  description = "PostgreSQL password"
+  type        = string
+}
