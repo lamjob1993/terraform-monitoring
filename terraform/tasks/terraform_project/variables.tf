36ffa90a4c2de7fd82179179c6354ff9caa8e760
lamjob1993 <43383780+lamjob1993@users.noreply.github.com>
2025-05-08 23:27:26 +0300

    Update variables.tf

--- terraform/tasks/terraform_project/variables.tf
+++ terraform/tasks/terraform_project/variables.tf
@@ -1,25 +1,20 @@
-variable "environments" {
-  description = "Map of environment configurations"
-  type = map(object({
-    web_port    = number
-    db_user     = string
-    db_password = string
-  }))
-  default = {
-    dev = {
-      web_port    = 8080
-      db_user     = "dev_user"
-      db_password = "dev_password"
-    }
-    staging = {
-      web_port    = 8081
-      db_user     = "staging_user"
-      db_password = "staging_password"
-    }
-    prod = {
-      web_port    = 8082
-      db_user     = "prod_user"
-      db_password = "prod_password"
-    }
-  }
+# Порт, на котором будет доступен веб-сервер (например, http://localhost:8080)
+variable "web_port" {
+  description = "External port for web server"
+  type        = number
+  default     = 8080
+}
+
+# Имя пользователя для PostgreSQL
+variable "db_user" {
+  description = "PostgreSQL user"
+  type        = string
+  default     = "dev_user"
+}
+
+# Пароль для PostgreSQL
+variable "db_password" {
+  description = "PostgreSQL password"
+  type        = string
+  default     = "dev_password"
 }
