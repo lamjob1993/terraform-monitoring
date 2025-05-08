823633b901d632d5c0c02c6620c632116c97be5f
lamjob1993 <43383780+lamjob1993@users.noreply.github.com>
2025-05-08 21:45:10 +0300

    Update outputs.tf

--- terraform/tasks/terraform_project/modules/environment/outputs.tf
+++ terraform/tasks/terraform_project/modules/environment/outputs.tf
@@ -1,1 +1,11 @@
+output "web_container_name" {
+  value = docker_container.web_server.name
+}
 
+output "db_container_name" {
+  value = docker_container.database.name
+}
+
+output "network_name" {
+  value = docker_network.env_network.name
+}
